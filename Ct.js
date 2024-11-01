0307a36c45fff64a433ee697803eaa9ff06b94a7
shehzad1212 <tawhidulislam261@gmail.com>
2024-11-01 14:37:12 +0600

    Final commitof password gen

--- Ct.js
+++ Ct.js
@@ -2,13 +2,13 @@
 
 // declare all characters
 const characters =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+|}{:/";
+  "A1BCDE2FGH3IJ4KLM5NOP6QRS7TUVW8XYZabc9defghijklmnopqrstuvwxyz~!@#$%^&*()_+|}{:/";
 
 function generateString() {
-  let result = " ";
+  let result = "";
   const charactersLength = characters.length;
 
-  for (let i = 0; i < charactersLength; i++) {
+  for (let i = 1; i < charactersLength; i++) {
     result =
       result + characters.charAt(Math.floor(Math.random() * charactersLength));
   }
@@ -17,24 +17,22 @@ function generateString() {
 }
 
 const getTheStr = generateString();
-const eleminateSameConsecutiveCharacters =
-    (inputData) => {
-        let output = "";
-        let lastChar = "";
+// We got the string
 
-        for (const letter of inputData) {
-            if (letter !== lastChar) {
-                output += letter;
-                lastChar = letter;
-            }
-        }
+const removeCharacter = (inputData) => {
+  let output = "";
+  let lastChar = "";
 
-        return output;
-    };
+  for (const letter of inputData) {
+    if (letter !== lastChar) {
+      output += letter;
+      lastChar = letter;
+    }
+  }
 
-const testString = "Geeks For Geeks";
-console.log(
-    eleminateSameConsecutiveCharacters(
-        testString
-    )
-);
+  return output;
+};
+
+const testString = getTheStr;
+const withOutRepeatChar = removeCharacter(testString);
+console.log(withOutRepeatChar.slice(0, 10));
