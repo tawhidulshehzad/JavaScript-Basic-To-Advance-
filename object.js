2c8aa57438f40eb03b40a595cb4634b5ce8d47af
shehzad1212 <tawhidulislam261@gmail.com>
2024-11-02 11:04:39 +0600

    object changing

--- object.js
+++ object.js
@@ -9,8 +9,21 @@ const subject = {
   name: "biology",
   teacher: "Rashed",
   chapter: ["firs", "second", "third"],
+  activities: {
+    social: "policies",
+    sports: "cricket",
+  },
+  color: "black",
 };
 // console.log(bottle['color']);
 // let newName = "name";
 subject.name = "abid";
+// console.log(subject);
+
+/* const keys = Object.keys(subject);
+console.log(keys);
+const values = Object.values(subject);
+console.log(values); */
+// delete subject["color"];
 console.log(subject);
+console.log(subject["activities"].sports);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-// console.log(characters);
+activities
