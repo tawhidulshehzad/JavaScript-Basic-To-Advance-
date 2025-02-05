cd0f283d1dca74abf32a7298238c8cb36538c2a6
shehzad1212 <tawhidulislam261@gmail.com>
2025-02-05 17:40:35 +0600

    print all number and sum

--- DSA/dataStructure01.js
+++ DSA/dataStructure01.js
@@ -1,2 +1,15 @@
-const string = ["a", "b", "c"];
-console.log(string);
+const numbers = [1, 2, 3, 4, 5];
+
+const printAllNumber = (numbers) => {
+  numbers.forEach((number) => {
+    console.log(number);
+  });
+  console.log("-----");
+  numbers.forEach((fNumber) => {
+    numbers.forEach((sNumber) => {
+      console.log(fNumber + sNumber);
+    });
+  });
+};
+
+printAllNumber(numbers);
